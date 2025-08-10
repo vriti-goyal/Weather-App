88e0e5af1a0947f7f6609fb00345a0acf982c1bf
vriti-goyal <143534909+vriti-goyal@users.noreply.github.com>
2025-08-10 16:10:24 +0530

    Update vite.config.js

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: 'Weather_App', // e.g. '/Weather-App/'
+  base: '/Weather-App/',   // <-- exact repo name, with slashes
 })
